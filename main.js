0817f7e2776ead733cee4b52e001dd5642b290a7
nikolastanin <nikolastanin@gmail.com>
2021-04-29 00:17:39 +0200

    fix

--- main.js
+++ main.js
@@ -11,22 +11,18 @@ let newNote = ()=>{
      
     //  check if empty and create div note elements
      if(textInput.value!=0){
-       let   p = document.createElement('p');
-      let   button = document.createElement('button');
+     let p = document.createElement('p');
+     let button = document.createElement('button');
      let noteElement = document.createElement("div");
      noteElement.setAttribute("class","noteElement");
-     
-        p.className ="note"
+     p.className ="note"
 //adding notes when add button clicked
         for(let i = 0; i<notes.length; i++){
            //add input value to div content
             p.innerHTML = notes[i];
             button.innerHTML = "delete"
           //adding custom IDs so we can delete exact notes 
-            //  div.id = "noteIndex"+[i];
-            noteElement.setAttribute("id","noteIndex"+i);
-            //  document.body.appendChild(div);
-            //  document.body.appendChild(button);
+             noteElement.setAttribute("id","noteIndex"+i);
              button.setAttribute("onclick","deleteNote("+i+")");
              button.setAttribute("id","button"+i);
             notesList.appendChild(noteElement);
@@ -46,12 +42,7 @@ let newNote = ()=>{
 //  Deleting a note   
 }
 let deleteNote = (n) =>{
- console.log(n);
-  let deleteNoteElement = document.getElementById("noteIndex"+n);
-  // let deleteButton = document.getElementById("button"+n)
-    // console.log(deleteNote);
-    // deleteNote.remove();
-    // deleteButton.remove();
+    let deleteNoteElement = document.getElementById("noteIndex"+n);
     deleteNoteElement.remove();
    }
 
